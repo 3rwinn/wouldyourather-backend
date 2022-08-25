37237261a29e0a04d96a662f978e514ee6b96c16
Erwinn Grebo <erwinngrebo@outlook.com>
2022-08-25 11:30:05 +0000

    fix: Add missing constraint to user sql model

--- migrations/sqls/20220823122536-users-up.sql
+++ migrations/sqls/20220823122536-users-up.sql
@@ -1,8 +1,9 @@
 /* Replace with your SQL commands */
 CREATE TABLE users(
     id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
+    name VARCHAR(55) NOT NULL,
+    email VARCHAR(55) UNIQUE NOT NULL,
+    password VARCHAR(60) NOT NULL,
     avatarUrl TEXT NOT NULL,
     questions JSON NULL,
     answers JSON NULL
