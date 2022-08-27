3296b184341d0739dbdfc3f88fe05ad57b3e3734
Erwinn Grebo <erwinngrebo@outlook.com>
2022-08-27 11:38:24 +0000

    feat: Add answer func to question model

--- src/models/question.ts
+++ src/models/question.ts
@@ -3,8 +3,23 @@ import client from "../database";
 export type Question = {
   id?: number;
   author: number;
+  text: string;
   optionOne: string;
   optionTwo: string;
+};
+
+export type Answer = {
+  id?: number;
+  question: number;
+  optionOne?: string;
+  optionTwo?: string;
+};
+
+export type QuestionWithAnswer = {
+  id?: number;
+  optionOne: string;
+  optionTwo: string;
+  result: Answer;
 };
 
 export class QuestionStore {
@@ -22,15 +37,22 @@ export class QuestionStore {
     }
   }
 
-  async show(id: number): Promise<Question> {
+  async show(id: number): Promise<QuestionWithAnswer> {
     try {
-      const sql = "SELECT * FROM questions where id=($1)";
       const conn = await client.connect();
 
+      const sql = "SELECT * FROM questions where id=($1)";
       const result = await conn.query(sql, [id]);
 
+      const retrieveAnswerSql = "SELECT * FROM answers where question=($1)";
+      const retrieveAnswer = await conn.query(retrieveAnswerSql, [id]);
+
       conn.release();
-      return result.rows[0];
+      const data = {
+        ...result.rows[0],
+        result: retrieveAnswer.rows[0],
+      };
+      return data;
     } catch (err) {
       throw new Error(`unable to show question ${id}: ${err}`);
     }
@@ -38,16 +60,19 @@ export class QuestionStore {
 
   async create(q: Question): Promise<Question> {
     try {
-      const sql =
-        "INSERT INTO questions (author, optionOne, optionTwo) VALUES($1, $2, $3) RETURNING *";
       const conn = await client.connect();
 
+      const sql =
+        "INSERT INTO questions (author, optionOne, optionTwo) VALUES($1, $2, $3) RETURNING *";
       const result = await conn.query(sql, [
         q.author,
         q.optionOne,
         q.optionTwo,
       ]);
       const question = result.rows[0];
+
+      const answerSql = "INSERT INTO answers (question) VALUES($1) RETURNING *";
+      await conn.query(answerSql, [question.id]);
 
       conn.release();
 
@@ -69,6 +94,28 @@ export class QuestionStore {
       return true;
     } catch (err) {
       throw new Error(`unable to delete question (${id}): ${err}`);
+    }
+  }
+
+  async addAnswer(
+    question_id: string,
+    optionOne?: string,
+    optionTwo?: string
+  ): Promise<Answer> {
+    try {
+      const sql =
+        "UPDATE answers SET optionOne=($2), optionTwo=($3) WHERE id=($1) RETURNING *";
+
+      const conn = await client.connect();
+
+      const result = await conn.query(sql, [question_id, optionOne, optionTwo]);
+      const answer = result.rows[0];
+
+      return answer;
+    } catch (err) {
+      throw new Error(
+        `unable to add answer to  question (${question_id}): ${err}`
+      );
     }
   }
 }
